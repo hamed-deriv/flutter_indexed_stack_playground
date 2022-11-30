5afd4bc93ff9815279c16422d9c2b678fb7b4df9
hamed-deriv <57184669+hamed-deriv@users.noreply.github.com>
2022-11-30 15:20:53 +0800

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const App());
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        title: 'Indexed Stack Playground',
+        theme: ThemeData(primarySwatch: Colors.blueGrey),
+        home: const HomePage(title: 'Indexed Stack Playground'),
+      );
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(elevation: 0, title: Text(widget.title)),
+        body: const Center(
+          child: Text('NO CONTENT YET!'),
+        ),
+      );
+}
