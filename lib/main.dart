69ba038d5bf1b337f694879b8324c71ab76b8384
hamed-deriv <57184669+hamed-deriv@users.noreply.github.com>
2022-12-01 09:41:22 +0800

    minor change

--- lib/main.dart
+++ lib/main.dart
@@ -23,11 +23,43 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int currentPageIndex = 0;
+
   @override
   Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(elevation: 0, title: Text(widget.title)),
-        body: const Center(
-          child: Text('NO CONTENT YET!'),
+        body: Center(
+          child: IndexedStack(
+            index: currentPageIndex,
+            children: <Widget>[
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const ListTile(
+                    tileColor: Colors.red,
+                    title: Center(
+                      child: Text('PAGE: 01'),
+                    ),
+                  ),
+                  TextButton(
+                    child: const Text('BUTTON'),
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.red),
+                    ),
+                    onPressed: () {},
+                  )
+                ],
+              ),
+              const Center(child: Text('PAGE: 02')),
+              const Center(child: Text('PAGE: 03')),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.book),
+          onPressed: () =>
+              setState(() => currentPageIndex = (currentPageIndex + 1) % 2),
         ),
       );
 }
